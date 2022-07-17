0ec09d2e0b3fd1d7798a46861e6b9be0d41b1476
Erwin Quek <72030222+erwinqxy@users.noreply.github.com>
2022-07-18 03:40:52 +0800

    Fix buttons

--- side-project/name-card/components/Links.jsx
+++ side-project/name-card/components/Links.jsx
@@ -3,15 +3,15 @@ import React from "react";
 export default function Links() {
   return (
     <div className="Links">
-      <b href="https://docs.google.com/document/d/1rQiFf5l_LW3wo7sDMSsfy9mGsUn1JN4a/edit?usp=sharing&ouid=111099942703627877902&rtpof=true&sd=true" target="_blank">
+      <a href="https://docs.google.com/document/d/1rQiFf5l_LW3wo7sDMSsfy9mGsUn1JN4a/edit?usp=sharing&ouid=111099942703627877902&rtpof=true&sd=true" target="_blank">
         <button> Resume </button>
-      </b>
-      <b href="https://github.com/erwinqxy" target="_blank">
+      </a>
+      <a href="https://github.com/erwinqxy" target="_blank">
         <button> Github </button>
-      </b>
-      <b href="https://erwinquek.vercel.app/" target="_blank">
+      </a>
+      <a href="https://erwinquek.vercel.app/" target="_blank">
         <button> Personal Site </button>
-      </b>
+      </a>
     </div>
   );
 }
--- side-project/name-card/readme.md
+++ side-project/name-card/readme.md
@@ -6,4 +6,4 @@ npm run dev
 ```
 
 ![Final](./src/assets/namecard.png)
-[Deployed To]()
+[Deployed To](https://erwin-namecard.vercel.app/)
